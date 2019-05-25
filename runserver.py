9ef128b0fd7be372d6a1084b5e8e93946080dd1a
Kumar Nityan Suman <nityan.suman@gmail.com>
2019-05-25 22:26:54 +0530

    Syntax Fix

--- runserver.py
+++ runserver.py
@@ -5,10 +5,10 @@ This script runs the ATP application using a development server.
 from os import environ
 from atp import app
 
-if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
+if __name__ == "__main__":
+    HOST = environ.get("SERVER_HOST", "localhost")
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
+        PORT = int(environ.get("SERVER_PORT", "5555"))
     except ValueError:
         PORT = 1234
     app.run(HOST, PORT, debug=True)
